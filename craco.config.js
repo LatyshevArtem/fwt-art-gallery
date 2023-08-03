99af7c71a9ed972c61d17b5cd5dba726f79fdbad
LatyshevArtem <92304016+LatyshevArtem@users.noreply.github.com>
2023-08-03 13:58:41 +0400

    Set up aliases. Added auto import breakpoints.scss and colors.scss to all scss files

---
+++ craco.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const getPathForAlias = (p) => path.resolve(__dirname, 'src', p);
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@assets': getPathForAlias('assets'),
+      '@components': getPathForAlias('components'),
+    }
+  },
+  style: {
+    sass: {
+      loaderOptions: {
+        implementation: require('sass'),
+        webpackImporter: false,
+        additionalData: `
+          @import './src/components/App/scss/breakpoints.scss';
+          @import './src/components/App/scss/colors.scss';
+        `,
+      },
+    },
+  },
+};
