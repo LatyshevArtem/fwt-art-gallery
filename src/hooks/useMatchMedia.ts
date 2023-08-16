3377d7d98ea128d9edcf406063489073bc2a02a0
LatyshevArtem <92304016+LatyshevArtem@users.noreply.github.com>
2023-08-16 11:50:59 +0400

    Renamed the interface, the variable with requests and removed the abbreviation in the callback function in accordance with the code style of the command

--- src/hooks/useMatchMedia.ts
+++ src/hooks/useMatchMedia.ts
@@ -1,35 +1,38 @@
 import { useState, useLayoutEffect } from 'react';
 
-interface IUseMatchMedia {
+interface UseMatchMedia {
   isMobile: boolean;
   isTablet: boolean;
   isDesktop: boolean;
 }
 
-const QUERIES = [
+const queries = [
   '(max-width: 767px)',
   '(min-width: 768px) and (max-width: 1439px)',
   '(min-width: 1440px)',
 ];
 
-const useMatchMedia = (): IUseMatchMedia => {
-  const mediaQueryLists = QUERIES.map((query) => matchMedia(query));
+const useMatchMedia = (): UseMatchMedia => {
+  const mediaQueryLists = queries.map((query) => matchMedia(query));
 
-  const getMatchesList = () => mediaQueryLists.map((mql) => mql.matches);
+  const getMatchesList = () => mediaQueryLists.map((mediaQueryList) => mediaQueryList.matches);
 
   const [matchesList, setMatchesList] = useState(getMatchesList);
 
   useLayoutEffect(() => {
     const handler = () => setMatchesList(getMatchesList);
 
-    mediaQueryLists.forEach((mql) => mql.addEventListener('change', handler));
+    mediaQueryLists.forEach((mediaQueryList) => mediaQueryList.addEventListener('change', handler));
 
-    return () => mediaQueryLists.forEach((mql) => mql.removeEventListener('change', handler));
+    return () =>
+      mediaQueryLists.forEach((mediaQueryList) =>
+        mediaQueryList.removeEventListener('change', handler),
+      );
   });
 
   return ['isMobile', 'isTablet', 'isDesktop'].reduce(
     (acc, screen, index) => ({ ...acc, [screen]: matchesList[index] }),
-    {} as IUseMatchMedia,
+    {} as UseMatchMedia,
   );
 };
 
