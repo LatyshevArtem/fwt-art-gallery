83d0830fcb5f2dc51de3ed18874f087549ed8920
LatyshevArtem <92304016+LatyshevArtem@users.noreply.github.com>
2023-08-16 11:46:27 +0400

    Removed an extra word in the function. Updated the paths in ThemeProvider.tsx. Renamed the files according to the new function names

--- src/providers/ThemeProvider/utils/checkIsUserPreferDarkColorScheme.ts
+++ src/providers/ThemeProvider/utils/checkIsUserPreferDarkColorScheme.ts
@@ -1,7 +1,5 @@
-const checkIsUserPrefersDarkColorScheme = () => {
+export const checkIsUserPrefersDarkColorScheme = () => {
   const mediaQueryList = matchMedia('(prefers-color-scheme)');
   const isUserPrefersDarkColorScheme = mediaQueryList.matches;
   return isUserPrefersDarkColorScheme;
 };
-
-export { checkIsUserPrefersDarkColorScheme };
