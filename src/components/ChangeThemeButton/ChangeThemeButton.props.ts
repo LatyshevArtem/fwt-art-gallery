76d42bd5e9276b48f391aa07426d79e4cd1bba2d
LatyshevArtem <92304016+LatyshevArtem@users.noreply.github.com>
2023-08-16 12:11:41 +0400

    Renamed the file with styles. Deleted a separate file with the component's props. Now its props are described in the component file. In the component file, updated the imports, removed unnecessary props and corrected the markup

--- src/components/ChangeThemeButton/ChangeThemeButton.props.ts
+++
@@ -1,6 +0,0 @@
-import { ButtonHTMLAttributes } from 'react';
-
-export interface ChangeThemeButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  isDarkTheme?: boolean;
-  shouldShowButtonText: boolean;
-}
