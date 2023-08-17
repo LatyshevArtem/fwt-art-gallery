426ce6482ec6baeb83ac92a3fb3ccb86c4562277
LatyshevArtem <92304016+LatyshevArtem@users.noreply.github.com>
2023-08-17 12:59:11 +0400

    Deleted the MobileMenu component. The Menu component was renamed to HeaderMenu for greater clarity. Renamed the component style files according to the component names.

--- src/components/layout/Header/Menu/Menu.props.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeThemeButtonProps } from '@components/ChangeThemeButton/ChangeThemeButton.props';
-import { MouseEventHandler } from 'react';
-
-export interface MenuProps extends ChangeThemeButtonProps {
-  onChangeThemeButtonClick: MouseEventHandler<HTMLButtonElement>;
-  onLogInButtonClick: MouseEventHandler<HTMLButtonElement>;
-  onSignUpButtonClick: MouseEventHandler<HTMLButtonElement>;
-}
--- src/components/layout/Header/MobileMenu/MobileMenu.props.ts
+++
@@ -1,7 +0,0 @@
-import { MouseEventHandler } from 'react';
-import { MenuProps } from '../Menu/Menu.props';
-
-export interface MobileMenuProps extends Omit<MenuProps, 'shouldShowButtonText'> {
-  isOpen: boolean;
-  onCloseButtonClick: MouseEventHandler<HTMLButtonElement>;
-}
