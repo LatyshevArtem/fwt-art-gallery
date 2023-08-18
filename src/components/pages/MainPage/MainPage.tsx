a3844dce51ad75b6b4aee90697eb2eb3ae916094
LatyshevArtem <92304016+LatyshevArtem@users.noreply.github.com>
2023-08-18 14:38:06 +0400

    Updated the MainPage component according to the components it uses. Added styles for this component

--- src/components/pages/MainPage/MainPage.tsx
+++ src/components/pages/MainPage/MainPage.tsx
@@ -1,25 +1,28 @@
-import Header from '@components/layout/Header';
+import { useContext, useEffect } from 'react';
+import cn from 'classnames/bind';
+import { ThemeContex } from '@contexts/ThemeContext';
+import { useAppSelector } from '@hooks/useAppSelector';
+import { useAppDispatch } from '@hooks/useAppDispatch';
+import Layout from '@components/layout/Layout/';
 import PaintingsSection from '@components/PaintingsSection';
-import Footer from '@components/layout/Footer';
-import PageContent from './PageContent';
+import { fetchArtists } from '../../../features/artistsSlice';
+import styles from './MainPage.module.scss';
 
-const paintings = [
-  {
-    imgSrc: 'https://internship-front.framework.team/images/64761919c25ef9fb3e0cdb95/image.jpg',
-    name: 'Jean-Honore Fragonard',
-    years: '1732 - 1806',
-  },
-];
+const cx = cn.bind(styles);
 
 const MainPage = () => {
+  const { isDarkTheme } = useContext(ThemeContex);
+  const artists = useAppSelector((state) => state.artists.artists);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchArtists());
+  }, [dispatch]);
+
   return (
-    <PageContent>
-      <Header />
-      <main>
-        <PaintingsSection paintings={[...new Array(6)].fill(paintings[0])} />
-      </main>
-      <Footer />
-    </PageContent>
+    <Layout className={cx('main-page', { 'main-page--dark': isDarkTheme })}>
+      <main>{artists && <PaintingsSection artists={artists} />}</main>
+    </Layout>
   );
 };
 
