10f0031d3e268e9d3bb94d6c7b2a3c82e32d80d5
LatyshevArtem <92304016+LatyshevArtem@users.noreply.github.com>
2023-08-18 14:40:32 +0400

    Updated the App component. And general application styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,16 @@
+import { Provider } from 'react-redux';
 import MainPage from '@components/pages/MainPage';
+import { store } from '../../store/store';
 import { ThemeProvider } from '../../providers/ThemeProvider';
 import './scss/index.scss';
 
 const App = () => {
   return (
-    <ThemeProvider>
-      <MainPage />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <MainPage />
+      </ThemeProvider>
+    </Provider>
   );
 };
 
