057766c64e63c5fac6b4c2fca0bf7ddea2f3dcc5
LatyshevArtem <92304016+LatyshevArtem@users.noreply.github.com>
2023-08-23 12:43:37 +0400

    Added the link to back

--- src/components/pages/ArtistPage/ArtistPage.tsx
+++ src/components/pages/ArtistPage/ArtistPage.tsx
@@ -1,13 +1,16 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useThemeContext } from '@hooks/useThemeContext';
+import { useMatchMedia } from '@hooks/useMatchMedia';
 import { useAppSelector } from '@hooks/useAppSelector';
 import { useAppDispatch } from '@hooks/useAppDispatch';
 import Layout from '@components/layout/Layout';
 import cn from 'classnames/bind';
+import Link from '@components/Link/Link';
 import ArtistCard from '@components/ArtistCard';
 import PaintingsGrid from '@components/PaintingsGrid';
 import PaintingCard from '@components/PaintingCard/PaintingCard';
+import { ReactComponent as BackArrowIcon } from '@assets/icons/arrow.svg';
 import { fetchArtistById } from '../../../features/artistByIdSlice';
 import styles from './ArtistPage.module.scss';
 
@@ -16,6 +19,7 @@ const cx = cn.bind(styles);
 const ArtistPage = () => {
   const { id } = useParams();
   const { isDarkTheme } = useThemeContext();
+  const { isMobile } = useMatchMedia();
   const artist = useAppSelector((state) => state.artistById.artist);
   const dispatch = useAppDispatch();
 
@@ -26,6 +30,12 @@ const ArtistPage = () => {
   return (
     <Layout className={cx('artist-page', { 'artist-page--dark': isDarkTheme })}>
       <main className={cx('artist-page__main')}>
+        <div className={cx('artist-page__back-link-wrapper')}>
+          <Link className={cx('artist-page__back-link')} isDarkTheme={isDarkTheme} to="..">
+            <BackArrowIcon className={cx('back-link__icon')} />
+            {!isMobile && <span className={cx('back-link__text')}>back</span>}
+          </Link>
+        </div>
         {artist && <ArtistCard artist={artist} />}
         <h1 className={cx('artist-page__heading', { 'artist-page__heading--dark': isDarkTheme })}>
           Artworks <span className="visually-hidden">by {artist?.name}</span>
