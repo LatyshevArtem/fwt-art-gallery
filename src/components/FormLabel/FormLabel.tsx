2bb83252892568d5eef250a4881b8cfe4f4908fa
LatyshevArtem <92304016+LatyshevArtem@users.noreply.github.com>
2023-08-24 14:45:14 +0400

    Added getting a unique identifier from the FormControl component

--- src/components/FormLabel/FormLabel.tsx
+++ src/components/FormLabel/FormLabel.tsx
@@ -1,5 +1,6 @@
 import { FC, LabelHTMLAttributes } from 'react';
 import cn from 'classnames/bind';
+import { useFormControlContext } from '@components/FormControl/FormControl';
 import styles from './FormLabel.module.scss';
 
 const cx = cn.bind(styles);
@@ -8,9 +9,16 @@ interface FormLabelProps extends LabelHTMLAttributes<HTMLLabelElement> {
   isDarkTheme?: boolean;
 }
 
-const FormLabel: FC<FormLabelProps> = ({ isDarkTheme, children, ...props }) => {
+const FormLabel: FC<FormLabelProps> = ({ isDarkTheme, children, htmlFor, ...props }) => {
+  const { id } = useFormControlContext();
+  const labelHtmlFor = htmlFor || id;
+
   return (
-    <label className={cx('form-label', { 'form-label--dark': isDarkTheme })} {...props}>
+    <label
+      className={cx('form-label', { 'form-label--dark': isDarkTheme })}
+      htmlFor={labelHtmlFor}
+      {...props}
+    >
       {children}
     </label>
   );
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,6 @@
 import { FC, InputHTMLAttributes } from 'react';
 import cn from 'classnames/bind';
+import { useFormControlContext } from '@components/FormControl/FormControl';
 import styles from './Input.module.scss';
 
 const cx = cn.bind(styles);
@@ -8,11 +9,20 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   isDarkTheme?: boolean;
 }
 
-const Input: FC<InputProps> = ({ isDarkTheme, placeholder = 'Placeholder', ...props }) => {
+const Input: FC<InputProps> = ({
+  isDarkTheme,
+  id: idProp,
+  placeholder = 'Placeholder',
+  ...props
+}) => {
+  const { id } = useFormControlContext();
+  const inputId = idProp || id;
+
   return (
     <input
       className={cx('input', { 'input--dark': isDarkTheme })}
       placeholder={placeholder}
+      id={inputId}
       {...props}
     />
   );
