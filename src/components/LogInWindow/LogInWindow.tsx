d932309545a38efbbb70ef499050edc1a2856cf2
LatyshevArtem <92304016+LatyshevArtem@users.noreply.github.com>
2023-08-28 17:38:09 +0400

    Updated the component according to the components it uses. Added state for email and password

--- src/components/LogInWindow/LogInWindow.tsx
+++ src/components/LogInWindow/LogInWindow.tsx
@@ -1,7 +1,9 @@
-import { FC } from 'react';
+import { ChangeEventHandler, FC, useState } from 'react';
 import { useThemeContext } from '@hooks/useThemeContext';
 import AuthWindow from '@components/AuthWindow';
 import AuthWindowLink from '@components/AuthWindowLink';
+
+type InputChangeEventHandler = ChangeEventHandler<HTMLInputElement>;
 
 interface LogInWindowProps {
   isOpen: boolean;
@@ -9,10 +11,27 @@ interface LogInWindowProps {
 }
 
 const LogInWindow: FC<LogInWindowProps> = ({ isOpen, onClose }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const { isDarkTheme } = useThemeContext();
 
+  const handleEmailChange: InputChangeEventHandler = (evt) => setEmail(evt.target.value);
+  const handlePasswordChange: InputChangeEventHandler = (evt) => setPassword(evt.target.value);
+
   return (
-    <AuthWindow windowType="login" isDarkTheme={isDarkTheme} isOpen={isOpen} onClose={onClose}>
+    <AuthWindow
+      windowType="login"
+      isDarkTheme={isDarkTheme}
+      isOpen={isOpen}
+      onClose={onClose}
+      authFormProps={{
+        email,
+        onEmailChange: handleEmailChange,
+        password,
+        onPasswordChange: handlePasswordChange,
+        onFormSubmit: () => {},
+      }}
+    >
       If you don&apos;t have an account yet, please{' '}
       <AuthWindowLink isDarkTheme={isDarkTheme} href="signup">
         sign up
--- src/components/SignUpWindow/SignUpWindow.tsx
+++ src/components/SignUpWindow/SignUpWindow.tsx
@@ -1,7 +1,9 @@
-import { FC } from 'react';
+import { ChangeEventHandler, FC, useState } from 'react';
 import { useThemeContext } from '@hooks/useThemeContext';
 import AuthWindow from '@components/AuthWindow';
 import AuthWindowLink from '@components/AuthWindowLink';
+
+type InputChangeEventHandler = ChangeEventHandler<HTMLInputElement>;
 
 interface SignUpWindowProps {
   isOpen: boolean;
@@ -9,10 +11,27 @@ interface SignUpWindowProps {
 }
 
 const SignUpWindow: FC<SignUpWindowProps> = ({ isOpen, onClose }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const { isDarkTheme } = useThemeContext();
 
+  const handleEmailChange: InputChangeEventHandler = (evt) => setEmail(evt.target.value);
+  const handlePasswordChange: InputChangeEventHandler = (evt) => setPassword(evt.target.value);
+
   return (
-    <AuthWindow windowType="signup" isDarkTheme={isDarkTheme} isOpen={isOpen} onClose={onClose}>
+    <AuthWindow
+      windowType="signup"
+      isDarkTheme={isDarkTheme}
+      isOpen={isOpen}
+      onClose={onClose}
+      authFormProps={{
+        email,
+        onEmailChange: handleEmailChange,
+        password,
+        onPasswordChange: handlePasswordChange,
+        onFormSubmit: () => {},
+      }}
+    >
       If you already have an account, please{' '}
       <AuthWindowLink isDarkTheme={isDarkTheme} href="login">
         log in
