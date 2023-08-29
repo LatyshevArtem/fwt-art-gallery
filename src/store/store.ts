52623c00bed6de3a5a02117d4cbf0ef4a04ab2a1
LatyshevArtem <92304016+LatyshevArtem@users.noreply.github.com>
2023-08-29 13:13:29 +0400

    Updated store.ts to the current state of the application

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import artistsReducer from '../features/artistsSlice';
-import artistByIdReducer from '../features/artistByIdSlice';
-import genresReducer from '../features/genresSlice';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { artistApi } from '@api/features';
 
 const store = configureStore({
   reducer: {
-    artists: artistsReducer,
-    artistById: artistByIdReducer,
-    genres: genresReducer,
+    [artistApi.reducerPath]: artistApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(artistApi.middleware),
 });
 
-type RootState = ReturnType<typeof store.getState>;
-type AppDispatch = typeof store.dispatch;
+setupListeners(store.dispatch);
 
 export { store };
-export type { RootState, AppDispatch };
