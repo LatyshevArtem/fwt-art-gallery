e6162449d486619d56a11468c0518f23b46f265a
LatyshevArtem <92304016+LatyshevArtem@users.noreply.github.com>
2023-09-05 09:29:01 +0400

    Added a request interceptor
    If a user sends a post request to an url that is intended for authorization, a device fingerprint will be automatically attached to it

--- src/http/http.ts
+++ src/http/http.ts
@@ -1,7 +1,27 @@
-import axios from 'axios';
+import axios, { InternalAxiosRequestConfig } from 'axios';
+import FingerprintJS from '@fingerprintjs/fingerprintjs';
 
 const http = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
 });
+
+const getFingerprint = async () => {
+  const fingerprint = await FingerprintJS.load();
+  const { visitorId } = await fingerprint.get();
+  return visitorId;
+};
+
+const handleRequestInterceptorSuccess = async (config: InternalAxiosRequestConfig) => {
+  const newConfig = config;
+
+  if (config.method === 'post' && config.url?.includes('auth')) {
+    const fingerprint = await getFingerprint();
+    newConfig.data.fingerprint = fingerprint;
+  }
+
+  return newConfig;
+};
+
+http.interceptors.request.use(handleRequestInterceptorSuccess);
 
 export { http };
