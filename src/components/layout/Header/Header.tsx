e3cd5e1deb45b789379f84f02d5006764197dfe1
LatyshevArtem <92304016+LatyshevArtem@users.noreply.github.com>
2023-09-05 13:02:43 +0400

    Linked the search parameter and the display of authorization windows
    Now the opening and closing of LogInWindow and SignUpWindow is accompanied by the addition of the search parameter auth=login or auth=signup, respectively

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,7 +1,9 @@
-import { useState, FC } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import cn from 'classnames/bind';
 import { useThemeContext } from '@hooks/useThemeContext';
 import { useMatchMedia } from '@hooks/useMatchMedia';
+import { AuthWindowType } from '@components/AuthWindow';
 import Logo from '@components/Logo';
 import { ReactComponent as BurgerMenuIcon } from '@assets/icons/menu-buger.svg';
 import Modal from '@components/Modal';
@@ -17,28 +19,53 @@ const Header: FC = () => {
   const [isSidePageOpen, setIsSidePageOpen] = useState(false);
   const [isLogInWindowOpen, setIsLogInWindowOpen] = useState(false);
   const [isSignUpWindowOpen, setIsSignUpWindowOpen] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
   const { isDarkTheme } = useThemeContext();
   const { isDesktop } = useMatchMedia();
 
   const openSidePage = () => setIsSidePageOpen(true);
   const closeSidePage = () => setIsSidePageOpen(false);
 
+  const deleteSearchParam = useCallback(
+    (name: string) => {
+      searchParams.delete(name);
+      setSearchParams(searchParams);
+    },
+    [searchParams, setSearchParams],
+  );
+
+  const appendSearchParam = (name: string, value: string) => {
+    searchParams.append(name, value);
+    setSearchParams(searchParams);
+  };
+
   const openLogInWindow = () => setIsLogInWindowOpen(true);
-  const closeLogInWindow = () => setIsLogInWindowOpen(false);
+  const closeLogInWindow = useCallback(() => {
+    deleteSearchParam('auth');
+    setIsLogInWindowOpen(false);
+  }, [deleteSearchParam]);
 
   const openSignUpWindow = () => setIsSignUpWindowOpen(true);
-  const closeSignUpWindow = () => setIsSignUpWindowOpen(false);
+  const closeSignUpWindow = useCallback(() => {
+    deleteSearchParam('auth');
+    setIsSignUpWindowOpen(false);
+  }, [deleteSearchParam]);
 
-  const handleAuthButtonClick = (button: 'login' | 'signup') => {
+  const handleAuthButtonClick = (button: AuthWindowType) => {
     return () => {
       closeSidePage();
-      if (button === 'login') {
-        openLogInWindow();
-      } else {
-        openSignUpWindow();
-      }
+      appendSearchParam('auth', button);
     };
   };
+
+  useEffect(() => {
+    const authWindowType = searchParams.get('auth') as AuthWindowType | null;
+    if (authWindowType === 'login') {
+      openLogInWindow();
+    } else if (authWindowType === 'signup') {
+      openSignUpWindow();
+    }
+  }, [searchParams]);
 
   return (
     <header className={cx('header', { 'header--dark': isDarkTheme })}>
