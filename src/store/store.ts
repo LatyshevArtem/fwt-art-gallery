eaa11cd72f51a38f9375c85a465f68519d8c61a6
LatyshevArtem <92304016+LatyshevArtem@users.noreply.github.com>
2023-09-05 10:02:23 +0400

    Added auth slice to the store. Also exported the store type and the type of dispatch function

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { apiService } from '@api/apiService';
+import auth from './features/auth/authSlice';
 
 const store = configureStore({
   reducer: {
     [apiService.reducerPath]: apiService.reducer,
+    auth,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiService.middleware),
 });
 
 setupListeners(store.dispatch);
 
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 export { store };
