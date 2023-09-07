9c7e1429bc89e452c16393ad7bfe21c8d9946579
LatyshevArtem <92304016+LatyshevArtem@users.noreply.github.com>
2023-09-07 09:41:35 +0400

    Added requests to get artists for an authorized user

--- src/api/features/artistApi.ts
+++ src/api/features/artistApi.ts
@@ -3,16 +3,40 @@ import { Artist } from '@schemas/Artist';
 import { ArtistById } from '@schemas/ArtistById';
 
 const urlStaticArtists = 'artists/static';
+const urlArtists = 'artists';
+
+interface ArtistsMeta {
+  count: number;
+  pageNumber: number;
+  perPage: number;
+}
 
 const artistApi = apiService.injectEndpoints({
   endpoints: (build) => ({
-    fetchArtists: build.query<Artist[], null>({
-      query: () => ({ url: urlStaticArtists }),
+    fetchArtists: build.query<{ artists: Artist[]; meta: ArtistsMeta | null }, boolean>({
+      query: (isAuth) => {
+        const destinationUrl = isAuth ? urlArtists : urlStaticArtists;
+        return {
+          url: destinationUrl,
+        };
+      },
+      transformResponse: (response: Artist[] | { data: Artist[]; meta: ArtistsMeta }) => {
+        const transformedResponse =
+          'data' in response
+            ? { artists: response.data, meta: response.meta }
+            : { artists: response, meta: null };
+        return transformedResponse;
+      },
     }),
-    fetchArtistById: build.query<ArtistById, string>({
-      query: (id: string) => ({ url: `${urlStaticArtists}/${id}` }),
+    fetchArtistById: build.query<ArtistById, { id: string; isAuth: boolean }>({
+      query: ({ id, isAuth }) => {
+        const destinationUrl = isAuth ? urlArtists : urlStaticArtists;
+        return {
+          url: `${destinationUrl}/${id}`,
+        };
+      },
     }),
   }),
 });
 
-export const { useFetchArtistsQuery, useFetchArtistByIdQuery } = artistApi;
+export const { useLazyFetchArtistsQuery, useLazyFetchArtistByIdQuery } = artistApi;
