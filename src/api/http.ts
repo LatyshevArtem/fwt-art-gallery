156a59831318b98fce4c7b40e30821d025c03b30
LatyshevArtem <92304016+LatyshevArtem@users.noreply.github.com>
2023-09-26 11:02:10 +0400

    Added automatic insertion of access token at put request

--- src/api/http.ts
+++ src/api/http.ts
@@ -10,7 +10,12 @@ const http = axios.create({
 const handleRequestInterceptorSuccess = async (config: InternalAxiosRequestConfig) => {
   const newConfig = config;
 
-  if (config.method === 'get' || config.method === 'delete' || config.method === 'post') {
+  if (
+    config.method === 'get' ||
+    config.method === 'delete' ||
+    config.method === 'post' ||
+    config.method === 'put'
+  ) {
     const accessToken = getTokenFromLocalStorage('access_token');
     if (accessToken) {
       config.headers.set('Authorization', `Bearer ${accessToken}`);
