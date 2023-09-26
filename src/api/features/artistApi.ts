c627024c5f2fdaae91fd71c6e1e95a407f1dd8ab
LatyshevArtem <92304016+LatyshevArtem@users.noreply.github.com>
2023-09-26 10:59:40 +0400

    Added a request to edit the artist. Also added tags for automatic re-fetch

--- src/api/features/artistApi.ts
+++ src/api/features/artistApi.ts
@@ -12,51 +12,70 @@ interface ArtistsMeta {
   perPage: number;
 }
 
-interface DataOfAddArtistRequest {
-  avatar?: Blob;
+export interface DataOfAddArtistRequest {
+  avatar?: File;
   name: string;
   yearsOfLife: string;
   description: string;
   genres: Genre[];
 }
 
-const artistApi = apiService.injectEndpoints({
-  endpoints: (build) => ({
-    fetchArtists: build.query<{ artists: Artist[]; meta: ArtistsMeta | null }, boolean>({
-      query: (isAuth) => {
-        const destinationUrl = isAuth ? urlArtists : urlStaticArtists;
-        return {
-          url: destinationUrl,
-        };
-      },
-      transformResponse: (response: Artist[] | { data: Artist[]; meta: ArtistsMeta }) => {
-        const transformedResponse =
-          'data' in response
-            ? { artists: response.data, meta: response.meta }
-            : { artists: response, meta: null };
-        return transformedResponse;
-      },
+const artistApi = apiService
+  .enhanceEndpoints({
+    addTagTypes: ['Artists', 'SpecificArtist'],
+  })
+  .injectEndpoints({
+    endpoints: (build) => ({
+      fetchArtists: build.query<{ artists: Artist[]; meta: ArtistsMeta | null }, boolean>({
+        query: (isAuth) => {
+          const destinationUrl = isAuth ? urlArtists : urlStaticArtists;
+          return {
+            url: destinationUrl,
+          };
+        },
+        transformResponse: (response: Artist[] | { data: Artist[]; meta: ArtistsMeta }) => {
+          const transformedResponse =
+            'data' in response
+              ? { artists: response.data, meta: response.meta }
+              : { artists: response, meta: null };
+          return transformedResponse;
+        },
+        providesTags: ['Artists'],
+      }),
+      fetchArtistById: build.query<ArtistById, { id: string; isAuth: boolean }>({
+        query: ({ id, isAuth }) => {
+          const destinationUrl = isAuth ? urlArtists : urlStaticArtists;
+          return {
+            url: `${destinationUrl}/${id}`,
+          };
+        },
+        providesTags: ['SpecificArtist'],
+      }),
+      addArtist: build.mutation<ArtistById, FormData>({
+        query: (data) => ({ method: 'POST', url: urlArtists, data }),
+        invalidatesTags: ['Artists'],
+      }),
+      editArtist: build.mutation<ArtistById, { id: string; data: FormData }>({
+        query: ({ id, data }) => {
+          return {
+            method: 'PUT',
+            url: `${urlArtists}/${id}`,
+            data,
+          };
+        },
+        invalidatesTags: ['SpecificArtist'],
+      }),
+      deleteArtistById: build.mutation<string, string>({
+        query: (id) => ({ method: 'DELETE', url: `${urlArtists}/${id}` }),
+        invalidatesTags: ['Artists'],
+      }),
     }),
-    fetchArtistById: build.query<ArtistById, { id: string; isAuth: boolean }>({
-      query: ({ id, isAuth }) => {
-        const destinationUrl = isAuth ? urlArtists : urlStaticArtists;
-        return {
-          url: `${destinationUrl}/${id}`,
-        };
-      },
-    }),
-    addArtist: build.mutation<ArtistById, DataOfAddArtistRequest>({
-      query: (data) => ({ method: 'POST', url: urlArtists, data }),
-    }),
-    deleteArtistById: build.mutation<string, string>({
-      query: (id) => ({ method: 'DELETE', url: `${urlArtists}/${id}` }),
-    }),
-  }),
-});
+  });
 
 export const {
   useLazyFetchArtistsQuery,
   useLazyFetchArtistByIdQuery,
   useAddArtistMutation,
+  useEditArtistMutation,
   useDeleteArtistByIdMutation,
 } = artistApi;
