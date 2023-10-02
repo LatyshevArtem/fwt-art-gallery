626f1aa960c76e0e01acc826c56047c69955aaba
LatyshevArtem <92304016+LatyshevArtem@users.noreply.github.com>
2023-10-02 14:51:00 +0400

    Updated components according to changes in hooks and the DeletionWindow component

--- src/components/Modal/ModalContent/ModalContent.tsx
+++ src/components/Modal/ModalContent/ModalContent.tsx
@@ -3,15 +3,22 @@ import { useOutsideClick } from '@hooks/useOutsideClick';
 import { useEscapeKeydown } from '@hooks/useEscapeKeydown';
 import { useModalContext } from '../Modal';
 
-interface ModalContentProps extends HTMLAttributes<HTMLDivElement> {}
+interface ModalContentProps extends HTMLAttributes<HTMLDivElement> {
+  shouldAttachHandlerOutsideEvents?: boolean;
+}
 
-const ModalContent: FC<ModalContentProps> = ({ children, className, ...props }) => {
+const ModalContent: FC<ModalContentProps> = ({
+  children,
+  className,
+  shouldAttachHandlerOutsideEvents = true,
+  ...props
+}) => {
   const modalRef = useRef<HTMLDivElement>(null);
 
   const { onClose } = useModalContext();
 
-  useOutsideClick(modalRef, onClose);
-  useEscapeKeydown(modalRef, onClose);
+  useOutsideClick(modalRef, onClose, shouldAttachHandlerOutsideEvents);
+  useEscapeKeydown(modalRef, onClose, shouldAttachHandlerOutsideEvents);
 
   return (
     <div className={className} ref={modalRef} {...props}>
